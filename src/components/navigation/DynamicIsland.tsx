7f4a33f5d43bc36bc3ad5e3885c515ad4170cba4
rahulnk04 <rahulnk04@gmail.com>
2026-09-23 21:49:39 +0530

    update

--- src/components/navigation/DynamicIsland.tsx
+++ src/components/navigation/DynamicIsland.tsx
@@ -1,2 +1,21 @@
-import BottomNavigation from "@mui/material/BottomNavigation";
-export const DynamicIsland = BottomNavigation;
+import React from "react";
+import MuiBottomNavigation, {
+  type BottomNavigationProps as MuiBottomNavigationProps,
+} from "@mui/material/BottomNavigation";
+
+export type DynamicIslandProps = MuiBottomNavigationProps;
+
+export const DynamicIsland = React.forwardRef<
+  HTMLDivElement,
+  DynamicIslandProps
+>(({ children, ...props }, ref) => {
+  const flatChildren = React.Children.toArray(children);
+  return (
+    <MuiBottomNavigation ref={ref} {...props}>
+      {flatChildren}
+    </MuiBottomNavigation>
+  );
+});
+
+DynamicIsland.displayName = "DynamicIsland";
+
