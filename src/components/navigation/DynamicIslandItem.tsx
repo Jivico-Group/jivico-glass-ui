eb77487e076f6c708acefe4ddbb2cfff00d2166b
rahulnk04 <rahulnk04@gmail.com>
2026-09-23 20:38:54 +0530

    update

--- src/components/navigation/DynamicIslandItem.tsx
+++ src/components/navigation/DynamicIslandItem.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from "react";
-import { Box } from "@mui/material";
+import { Box, SxProps } from "@mui/material";
 import type { ElementType, ReactNode } from "react";
 
 export interface BottomNavigationItemProps {
@@ -17,7 +17,7 @@ export interface BottomNavigationItemProps {
   /**
    * Custom styling override
    */
-  sx?: any;
+  sx?: SxProps;
 
   [key: string]: any;
 }
